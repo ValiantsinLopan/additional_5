b4d8b4d4b0c9516c95a94398786bf9b526816934
Valentin Lopan <valiantsin_lopan@live.com>
2018-09-29 23:17:02 +0300

    implement check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let config = bracketsConfig.join("").replace(/,/g, '');
+
+  for (let i = 0; i < str.length; i++) {
+    let bracket = str[i];
+    let indexOfBracket = config.indexOf(bracket);
+    let top = stack[stack.length - 1];
+
+    if (stack.length > 0 && indexOfBracket % 2 == 0 && bracket == config[indexOfBracket + 1] && top == indexOfBracket) {
+      stack.pop();
+      continue;
+    }
+
+    if (indexOfBracket % 2 == 0) {
+      stack.push(indexOfBracket);
+    } else {
+      if (stack.length == 0) {
+        return false;
+      }
+      if (stack[stack.length - 1] + 1 == indexOfBracket) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length == 0;
 }
